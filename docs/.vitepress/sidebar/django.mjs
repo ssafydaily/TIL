845038d23764b6c7dd55a805c9320d5e47b718a4
jongyun <jongyun.jung@gmail.com>
2024-05-13 13:46:34 +0900

    move md file django to example

--- docs/.vitepress/sidebar/django.mjs
+++ docs/.vitepress/sidebar/django.mjs
@@ -5,11 +5,6 @@ export const django = {
     items: [
       { text: '소개', link: '/django/' },        
       { text: '요청과 응답', link: '/django/request_response' },        
-      { text: 'CORS', link: '/django/cors' },        
-      { text: 'pagination', link: '/django/pagination' },        
-      { text: 'tmdb', link: '/django/tmdb' },        
-      { text: 'dj-rest-auth', link: '/django/dj_rest' },        
-      { text: 'jwt', link: '/django/jwt' },        
     ]
   }
 }
--- docs/.vitepress/sidebar/pjt.mjs
+++ docs/.vitepress/sidebar/pjt.mjs
@@ -1,9 +1,21 @@
+const vueWithDjango = {
+  text: 'Vue + DRF',
+  items: [    
+    { text: 'CORS', link: '/examples/cors' },        
+    { text: 'pagination', link: '/examples/pagination' },        
+    { text: 'tmdb', link: '/examples/tmdb' },        
+    { text: 'dj-rest-auth', link: '/examples/dj_rest' },        
+    { text: 'jwt', link: '/examples/jwt' },        
+  ]
+}
+
 export const pjt = {
   '/examples/': {
-    text: 'PJT',
+    text: '결합',
     collapsed: true,
     items: [
-      { text: '소개', link: '/examples/' },          
+      { text: '소개', link: '/examples/' },      
+      vueWithDjango,    
     ]
   },
 }
